44235df7ea34476fcd9bc2201b591093df85800d
Enrico Hoffmann <enrico.hoffmann@movingimage24.com>
2014-10-27 17:19:14 +0100

    try 001

--- mi24/player/dist/common/services/services.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  define(['angular', 'common/services/HttpService', 'common/services/VideoPlayerCollection', 'common/services/UtilService'], function(angular, HttpService, VideoPlayerCollection, UtilService) {
-    'use strict';
-    return angular.module('Mi24HTMLPlayer.Common.Services', []).factory('HttpService', HttpService).service('UtilService', UtilService).service('VideoPlayerCollection', VideoPlayerCollection);
-  });
-
-}).call(this);
--- mi24/player/player.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  define(['angular', 'common/directives/directives', 'common/services/services', 'config/ConfigModule', 'ad/AdModule'], function(angular, CommonDirectives, CommonServices, ConfigModule, AdModule) {
-    'use strict';
-    return angular.module('Mi24HTMLPlayer', ['Mi24HTMLPlayer.Config', 'Mi24HTMLPlayer.Ad', 'Mi24HTMLPlayer.Common.Directives', 'Mi24HTMLPlayer.Common.Services']);
-  });
-
-}).call(this);
